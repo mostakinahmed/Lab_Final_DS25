6cf1dd8d88e5c6b8b8f3079c2700ca62f769f2bc
mostakinahmed <mostakin4556@gmail.com>
2025-08-03 08:31:48 +0600

    circular queue

---
+++ Task-4/main.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+int isEmpty();
+int isFull();
+void enqueue(int data);
+void display();
+
+#define Max 5
+int front = -1, rear = -1, size = Max;
+int data[Max];
+
+// initialize function
+int isEmpty()
+{
+    if (front == -1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int isFull()
+{
+    if ((front == 0 && rear == size - 1) || (front == (rear + 1) % size))
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+void enqueue(int value)
+{
+    if (isFull())
+    {
+        printf("Queue is Full.");
+        return;
+    }
+    else
+    {
+        if (front == -1)
+        {
+            front = front + 1;
+        }
+
+        rear = (rear + 1) % size;
+        data[rear] = value;
+        printf("%d is inserted.\n", value);
+    }
+}
+
+void display()
+{
+
+    if (isEmpty())
+    {
+        printf("Empty Queue.");
+    }
+    else
+    {
+        int i = front;
+        while (1)
+        {
+            printf("%d ", data[i]);
+            if (i == rear){
+                break;
+            }
+            i = (i + 1) % size;
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    enqueue(10);
+    enqueue(20);
+    display();
+}
